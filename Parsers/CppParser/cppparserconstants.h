caf6622d7454f6e5aa1a0592196b575cb456f9a8
Peter Nordin <peterNordin@users.noreply.github.com>
2015-09-08 10:49:33 +0200

    Added .cc to the list of C++ file name extensions

--- Parsers/CppParser/cppparserconstants.h
+++ Parsers/CppParser/cppparserconstants.h
@@ -33,7 +33,7 @@ const char WEBSITE_ADDRESS_REGEXP_PATTERN[] = "((http(s)?):\\/\\/)?(\\w+\\.){1,1
 /* Pattern for characters that can be part of a website URL. */
 const char WEBSITE_CHARS_REGEXP_PATTERN[]   = "[/:\\?=#%-]";
 /* Pattern for CPP files. */
-const char CPP_SOURCE_FILES_REGEXP_PATTERN[]= ".*\\.(c|cpp|h|hpp|dox)$";
+const char CPP_SOURCE_FILES_REGEXP_PATTERN[]= ".*\\.(c|cc|cpp|h|hpp|dox)$";
 
 const char CPP_PARSER_GROUP[]       = "CppParser";
 const char WHAT_TO_CHECK[]          = "WhatToCheck";
