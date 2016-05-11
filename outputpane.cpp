6cca0db57a4091d90ad5a82f8c72c341b3d67d19
Carel Combrink <carel.combrink@gmail.com>
2016-05-11 23:16:49 +0200

    Fixed copy-paste error.

--- outputpane.cpp
+++ outputpane.cpp
@@ -100,7 +100,7 @@ OutputPane::OutputPane(SpellingMistakesModel *model, QObject *parent) :
     connect(d->buttonSuggest, &QAbstractButton::clicked, SpellCheckerCore::instance(), &SpellCheckerCore::giveSuggestionsForWordUnderCursor);
 
     d->buttonIgnore = new QToolButton();
-    d->buttonAdd->setIcon(Core::Icons::MINUS.icon());
+    d->buttonIgnore->setIcon(Core::Icons::MINUS.icon());
     d->buttonIgnore->setToolTip(tr("Ignore the word"));
     d->toolbarWidgets.push_back(d->buttonIgnore);
     connect(d->buttonIgnore, &QAbstractButton::clicked, SpellCheckerCore::instance(), &SpellCheckerCore::ignoreWordUnderCursor);
