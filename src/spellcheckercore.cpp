zeule/qtc-SpellChecker-Plugin

Add option to set error color

src/spellcheckercore.cpp
@@ -244,8 +244,7 @@ void SpellCheckerCore::addMisspelledWords( const QString& fileName, const WordLi
      * and color stays the same, we just want to underline the mistake. */
     QTextCharFormat format = cursor.charFormat();
     format.setFontUnderline( true );
-    static const QColor underLineColor = QColor( Qt::red );
-    format.setUnderlineColor( underLineColor );
+    format.setUnderlineColor( d->settings.errorsColor );
     format.setUnderlineStyle( QTextCharFormat::WaveUnderline );
     format.setToolTip( word.suggestions.isEmpty()
                        ? QStringLiteral( "Incorrect spelling" )

src/spellcheckercoreoptionswidget.cpp
@@ -60,7 +60,7 @@ private slots:
     void toolButtonRemoveProjectClicked();
     void listWidgetItemChanged( QListWidgetItem* item );
 
-private:
+  private:
     void updateWithSettings( const SpellCheckerCoreSettings* const settings );
 
     Ui::SpellCheckerCoreOptionsWidget ui;
@@ -96,6 +96,7 @@ SpellCheckerCoreOptionsWidget::SpellCheckerCoreOptionsWidget( SpellCheckerCoreSe
    * is for the toolbar since nowhere else is a normal one needed. */
   ui.toolButtonRemoveProject->setIcon( Utils::Icon( { { ":/utils/images/minus.png"
                                                        , Utils::Theme::PaletteText } }, Utils::Icon::Tint ).icon() );
+  ui.buttonErrorsColor->setAlphaAllowed( false );
 
   const QMap<QString, ISpellChecker*> availableSpellCheckers = SpellCheckerCore::instance()->addedSpellCheckers();
   // todo: change QMap to std::map and use std::views::keys
@@ -129,6 +130,7 @@ SpellCheckerCoreSettings SpellCheckerCoreOptionsWidget::settings()
   settings.checkExternalFiles       = ui.checkBoxCheckExternal->isChecked();
   settings.projectsToIgnore         = m_projectsToIgnore;
   settings.replaceAllFromRightClick = ui.checkBoxReplaceAllRightClick->isChecked();
+  settings.errorsColor              = ui.buttonErrorsColor->color();
   return settings;
 }
 // --------------------------------------------------
@@ -180,6 +182,7 @@ void SpellCheckerCoreOptionsWidget::updateWithSettings( const SpellCheckerCoreSe
   ui.listWidget->clear();
   ui.listWidget->addItems( m_projectsToIgnore );
   ui.checkBoxReplaceAllRightClick->setChecked( settings->replaceAllFromRightClick );
+  ui.buttonErrorsColor->setColor( settings->errorsColor );
 }
 // --------------------------------------------------
 

src/spellcheckercoresettings.h
@@ -22,6 +22,7 @@
 
 #include <utils/qtcsettings.h>
 
+#include <QColor>
 #include <QObject>
 
 namespace SpellChecker {
@@ -43,6 +44,7 @@ public:
   bool operator==( const SpellCheckerCoreSettings& other ) const;
 
   QString activeSpellChecker;
+  QColor errorsColor{ Qt::red };
   bool onlyParseCurrentFile;
   bool checkExternalFiles;
   QStringList projectsToIgnore;

CJCombrink/SpellChecker-Plugin

src/spellcheckercore.cpp
@@ -244,8 +244,7 @@ void SpellCheckerCore::addMisspelledWords( const QString& fileName, const WordLi
      * and color stays the same, we just want to underline the mistake. */
     QTextCharFormat format = cursor.charFormat();
     format.setFontUnderline( true );
-    static const QColor underLineColor = QColor( Qt::red );
-    format.setUnderlineColor( underLineColor );
+    format.setUnderlineColor( d->settings.underlineColor );
     format.setUnderlineStyle( QTextCharFormat::WaveUnderline );
     format.setToolTip( word.suggestions.isEmpty()
                        ? QStringLiteral( "Incorrect spelling" )

src/spellcheckercoreoptionswidget.cpp
@@ -60,7 +60,7 @@ private slots:
     void toolButtonRemoveProjectClicked();
     void listWidgetItemChanged( QListWidgetItem* item );
 
-private:
+  private:
     void updateWithSettings( const SpellCheckerCoreSettings* const settings );
 
     Ui::SpellCheckerCoreOptionsWidget ui;
@@ -96,6 +96,7 @@ SpellCheckerCoreOptionsWidget::SpellCheckerCoreOptionsWidget( SpellCheckerCoreSe
    * is for the toolbar since nowhere else is a normal one needed. */
   ui.toolButtonRemoveProject->setIcon( Utils::Icon( { { ":/utils/images/minus.png"
                                                        , Utils::Theme::PaletteText } }, Utils::Icon::Tint ).icon() );
+  ui.buttonUnderlineColor->setAlphaAllowed( false );
 
   const QMap<QString, ISpellChecker*> availableSpellCheckers = SpellCheckerCore::instance()->addedSpellCheckers();
   // todo: change QMap to std::map and use std::views::keys
@@ -129,6 +130,7 @@ SpellCheckerCoreSettings SpellCheckerCoreOptionsWidget::settings()
   settings.checkExternalFiles       = ui.checkBoxCheckExternal->isChecked();
   settings.projectsToIgnore         = m_projectsToIgnore;
   settings.replaceAllFromRightClick = ui.checkBoxReplaceAllRightClick->isChecked();
+  settings.underlineColor           = ui.buttonUnderlineColor->color();
   return settings;
 }
 // --------------------------------------------------
@@ -180,6 +182,7 @@ void SpellCheckerCoreOptionsWidget::updateWithSettings( const SpellCheckerCoreSe
   ui.listWidget->clear();
   ui.listWidget->addItems( m_projectsToIgnore );
   ui.checkBoxReplaceAllRightClick->setChecked( settings->replaceAllFromRightClick );
+  ui.buttonUnderlineColor->setColor( settings->underlineColor );
 }
 // --------------------------------------------------
 

src/spellcheckercoresettings.h
@@ -22,6 +22,7 @@
 
 #include <utils/qtcsettings.h>
 
+#include <QColor>
 #include <QObject>
 
 namespace SpellChecker {
@@ -43,6 +44,7 @@ public:
   bool operator==( const SpellCheckerCoreSettings& other ) const;
 
   QString activeSpellChecker;
+  QColor underlineColor{ Qt::red };
   bool onlyParseCurrentFile;
   bool checkExternalFiles;
   QStringList projectsToIgnore;